86c9c22b065ffa94ffd5571011fd6702e7baecfc
Cbinh0710 <cbinh071002@gmail.com>
2024-07-19 16:01:20 +0700

    Format completely folder structure

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const { readFileSync } = require('fs');
-const posthtml = require('posthtml');
-const include = require('posthtml-include');
-
-const html = readFileSync('index.html', 'utf8');
-
-posthtml([ include({ encoding: 'utf8' }) ])
-    .process(html)
-    .then((result) => console.log(result.html));
